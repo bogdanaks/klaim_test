521b653e335f8f51ffb6090f7f96b73e6a1c6d65
bogdanaks <bogdanaks@bk.ru>
2023-01-24 01:40:33 +0300

    Protect route

--- src/models/quote/quote.controller.ts
+++ src/models/quote/quote.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, UseInterceptors } from "@nestjs/common"
+import {
+  Controller,
+  Get,
+  Query,
+  UseGuards,
+  UseInterceptors,
+  ValidationPipe
+} from "@nestjs/common"
+import { LocalAuthGuard } from "src/models/auth/local.auth.guard"
 import { ResponseInterceptor } from "src/common/response.interceptor"
+import { delay } from "src/common/utils"
+import { GetQuoteDTO } from "./quote.dto"
 import { QuoteService } from "./quote.service"
 
 @UseInterceptors(ResponseInterceptor)
@@ -10,12 +20,26 @@ import { QuoteService } from "./quote.service"
 export class QuoteController {
   constructor(private readonly quoteService: QuoteService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Get()
-  getQuote(): {
+  async getQuote(
+    @Query(new ValidationPipe())
+    data: GetQuoteDTO
+  ): Promise<{
     authorId: number
     quoteId: number
     quote: string
-  } {
-    return this.quoteService.getQuote()
+  }> {
+    const quote = await this.quoteService.getQuoteByAuthorId(
+      Number(data.authorId)
+    )
+
+    await delay(5000)
+
+    return {
+      authorId: quote.author_id,
+      quoteId: quote.id,
+      quote: quote.quote
+    }
   }
 }
