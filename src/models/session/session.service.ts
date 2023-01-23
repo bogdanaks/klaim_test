ecb5dc6d1a2ebf63d9a62a8e2dab6177495939da
bogdanaks <bogdanaks@bk.ru>
2023-01-24 01:39:40 +0300

    Create service

---
+++ src/models/session/session.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository } from "typeorm"
+import { Session } from "./session.entity"
+
+@Injectable()
+export class SessionService {
+  constructor(
+    @InjectRepository(Session)
+    public sessionRepository: Repository<Session>
+  ) {}
+
+  getUserSessionByToken(token: string): Promise<Session> {
+    return this.sessionRepository.findOne({ where: { id: token } })
+  }
+
+  async deleteUserSessionByToken(token: string): Promise<void> {
+    await this.sessionRepository.delete({ id: token })
+  }
+}
