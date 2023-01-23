7ce508198c4315d5310541594764f7b505080aaf
bogdanaks <bogdanaks@bk.ru>
2023-01-24 01:35:53 +0300

    Include modules

--- src/models/author/author.module.ts
+++ src/models/author/author.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
+import { TypeOrmModule } from "@nestjs/typeorm"
 import { AuthorController } from "./author.controller"
+import { Author } from "./author.entity"
 import { AuthorService } from "./author.service"
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Author]), ConfigModule],
   controllers: [AuthorController],
   providers: [AuthorService]
 })
